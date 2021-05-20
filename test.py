90a75283c59b568ed28c3dcb53cfbbf92a8f02c7
FVargas4 <fvargascode@gmail.com>
2021-05-19 22:58:53 -0500

    Agregar abrir vs code en carpeta seleccionada

--- test.py
+++ test.py
@@ -12,22 +12,30 @@ interruptor = 0
 
 print("El programa inicia a las " + time.ctime())
 
-interruptor = int(input('Cual es el numero magico? '))
+interruptor = int(input('Que vamos a hacer hoy? '))
 
-if(interruptor == 4):
+
+#with switch(interruptor) as s:
+if interruptor == 4:
     print('Le atinaste')
     time.sleep(1)
-    """ webbrowser.open("https://drive.google.com/drive/u/1/folders/0ABfuvAAzZODCUk9PVA")
-    webbrowser.open_new('https://www.youtube.com/channel/UCSM3FVwdCIJfU0OdjKZb94A') """
-    #os.system('code')
-    os.system('echo %cd%')
+    webbrowser.open("https://drive.google.com/drive/u/1/folders/0ABfuvAAzZODCUk9PVA")
+    webbrowser.open_new('https://www.youtube.com/channel/UCSM3FVwdCIJfU0OdjKZb94A')
+    webbrowser.open_new_tab("localhost")
     os.chdir('..')
-    os.chdir('ProyectoBloqueII/Ninosyninas')
-    os.system('code')
-    os.system('echo %cd%')
+    os.system('code -g ProyectoBloqueII/Ninosyninas')
+    totalProgramas = 4
+
+    
+
+    
     
     
 
 totalProgramas = str(totalProgramas)
 
 print("Se han abierto " + totalProgramas + " programas" )
+
+
+def switchPrograma(a):
+    print('empieza el switch')
