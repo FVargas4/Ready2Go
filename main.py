f407dc057f9f4c1d16defa84f3f54f0ceb33a7b7
FVargas4 <fvargascode@gmail.com>
2021-05-25 12:22:14 -0500

    Correcciones necesarias y agregar seleccion de dispositivo mac o pc

--- main.py
+++ main.py
@@ -7,17 +7,22 @@ totalProgramas = 0
 interruptor = 0
 exit = 0
 print("El programa inicia a las " + time.ctime())
-
+device = str(input('Estas en Mac o PC? ', ))
+if device == 'mac' or device == 'Mac' or device == '1':
+    dvs = 1
+elif device == 'pc' or device == 'PC' or device == '2':
+    dvs = 2
 interruptor = str(input('¿Qué vamos a hacer hoy? '))
 while(exit == 0):
 
-    os.chdir('/Users/fhva/Documents')
+    #os.chdir('/Users/fhva/Documents')
     if interruptor == 'Salir' or interruptor == 'No' or interruptor == 'no':
         print('Gracias por usar Ready 2 Go')
         exit = 1
         break
     else:
         print('Buena suerte con ' + interruptor)
+        totalProgramas = 0
 
 
     if interruptor == 'bloque' or interruptor == 'Bloque':
@@ -25,30 +30,48 @@ while(exit == 0):
         webbrowser.open_new("https://drive.google.com/drive/u/1/folders/0ABfuvAAzZODCUk9PVA")
         time.sleep(2)
         webbrowser.open_new_tab('https://www.youtube.com/watch?v=bmVKaAV_7-A')
-        os.chdir('ProyectoBloqueII')
-        os.chdir('Ninosyninas')
+        if dvs == 1:
+            os.chdir('ProyectoBloqueII')
+            os.chdir('Ninosyninas')
+        elif dvs == 2:
+            os.chdir(r'C:/Users/ferva/Documents/ProyectoBloqueII/Ninosyninas')
         os.system('code .')
-        os.chdir('..')
-        os.chdir('..')
+        if dvs ==1:
+            os.chdir('/Users/fhva/Documents')
+        elif dvs ==2:
+            os.chdir(r'C:/Users/ferva/Documents')
         totalProgramas = 3
         
 
     elif interruptor == 'R2G':
         print('Lanzando sus programas...')
-        os.system('open -a "/Applications/Notion.app"')
+        
+        if dvs ==1:
+            os.system('open -a "/Applications/Notion.app"')
+        elif dvs ==2:
+            print('Falta investigar como abrir Notion desde PC')
         time.sleep(2)
         webbrowser.open_new('https://www.youtube.com/watch?v=bmVKaAV_7-A')
         webbrowser.open_new('https://www.google.com/')
-        os.chdir('Ready2Go')
+        if dvs ==1:
+            os.chdir('/Users/fhva/Documents/Ready2Go')
+        elif dvs ==2:
+            os.chdir(r'C:/Users/ferva/Documents/Ready2Go')
         os.system('code .')
-        os.chdir('..')
         totalProgramas = 4
 
     elif interruptor == 'Relax' or interruptor == 'relax':
-        os.system('open -a "/Applications/Spotify.app"')
+        if dvs ==1:
+            os.system('open -a "/Applications/Spotify.app"')
+        elif dvs ==2:
+            print('Falta investigar como abrir Spotify desde PC')
         webbrowser.open_new('https://www.youtube.com')
+        webbrowser.open_new('https://www.twitch.tv')
         totalProgramas = 2    
 
+    else:
+        print('Comando no reconocido por el sistema, intente otra vez.')
+        continue
     totalProgramas = str(totalProgramas)
 
     print("Se han abierto " + totalProgramas + " programas" )
